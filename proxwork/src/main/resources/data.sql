63defe43fa29918aa50f55a383da8c732f1d7a9d
Caua Costa <caualcosta0408@gmail.com>
2026-03-03 17:41:28 -0300

    Preparando projeto para README

---
+++ proxwork/src/main/resources/data.sql
@@ -0,0 +1,20 @@
+--
+-- Data for Name: servico; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+INSERT INTO public.servico (id, deletado, descricao) VALUES ( 1,  false, 'Refrigeração');
+INSERT INTO public.servico (id, deletado, descricao) VALUES ( 2,  false, 'Elétricos');
+INSERT INTO public.servico (id, deletado, descricao) VALUES ( 3,  false, 'Hidráulicos');
+INSERT INTO public.servico (id, deletado, descricao) VALUES ( 4,  false, 'Reparo e manutenção');
+INSERT INTO public.servico (id, deletado, descricao) VALUES ( 5,  false, 'Limpeza');
+INSERT INTO public.servico (id, deletado, descricao) VALUES ( 6,  false, 'Construção e reforma');
+INSERT INTO public.servico (id, deletado, descricao) VALUES ( 7,  false, 'Design e decoração');
+INSERT INTO public.servico (id, deletado, descricao) VALUES ( 8,  false, 'Jardinagem');
+INSERT INTO public.servico (id, deletado, descricao) VALUES ( 9,  false, 'Automotivos');
+INSERT INTO public.servico (id, deletado, descricao) VALUES ( 10, false,  'Mudança e transporte');
+INSERT INTO public.servico (id, deletado, descricao) VALUES ( 11, false,  'Educacionais');
+INSERT INTO public.servico (id, deletado, descricao) VALUES ( 12, false,  'Tecnologia');
+INSERT INTO public.servico (id, deletado, descricao) VALUES ( 13, false,  'Pessoais e familiares');
+INSERT INTO public.servico (id, deletado, descricao) VALUES ( 14, false,  'Festas e eventos');
+INSERT INTO public.servico (id, deletado, descricao) VALUES ( 15, false,  'Profissionais');
+INSERT INTO public.servico (id, deletado, descricao) VALUES ( 16, false,  'Costura e moda');
